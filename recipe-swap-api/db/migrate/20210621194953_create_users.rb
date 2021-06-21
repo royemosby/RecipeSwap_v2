fc6459548740d6626249b711ae77f04c93d40dbb
Roy Mosby <ego.madking@gmail.com>
2021-06-21 16:33:07 -0400

    creates user

---
+++ recipe-swap-api/db/migrate/20210621194953_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :username
+      t.string :password_digest
+      t.text :about
+      t.string :profile_image
+
+      t.timestamps
+    end
+  end
+end
