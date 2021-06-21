8cbce55fa61b0c7d917738d823e5bb661ea00ea7
Roy Mosby <ego.madking@gmail.com>
2021-06-21 16:47:55 -0400

    adds recipe table

--- recipe-swap-api/db/schema.rb
+++ recipe-swap-api/db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_194953) do
+ActiveRecord::Schema.define(version: 2021_06_21_204411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "recipes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "ingredients"
+    t.text "instructions"
+    t.integer "servings"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "username"
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_06_21_194953) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
