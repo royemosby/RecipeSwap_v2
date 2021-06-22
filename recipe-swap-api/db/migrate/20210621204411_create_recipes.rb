b742727dd80e53d20045717e56ce9a669486a392
Roy Mosby <ego.madking@gmail.com>
2021-06-21 21:14:24 -0400

    updates for self-referential association

--- recipe-swap-api/db/migrate/20210621204411_create_recipes.rb
+++ recipe-swap-api/db/migrate/20210621204411_create_recipes.rb
@@ -7,7 +7,8 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
       t.text :instructions
       t.integer :servings
       t.references :user, null: false, foreign_key: true, type: :uuid
-
+      t.references :original, null: true, foreign_key: {to_table: :recipes}, type: :uuid
+      
       t.timestamps
     end
   end
