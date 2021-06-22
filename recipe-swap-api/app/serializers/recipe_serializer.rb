20326fc31b42bf36650c3a528651f085242872ff
Roy Mosby <ego.madking@gmail.com>
2021-06-22 16:16:42 -0400

    adds model relationship

--- recipe-swap-api/app/serializers/recipe_serializer.rb
+++ recipe-swap-api/app/serializers/recipe_serializer.rb
@@ -1,4 +1,11 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :ingredients, :instructions, :servings
-  has_one :user
+  # has_one :user
+
+  belongs_to :user
+  belongs_to :original, class_name: "Recipe", optional: true
+  has_many :spinoffs, class_name: "Recipe", foreign_key: "original_id"
+  # Not needed
+  # has_many :collector_favorites, foreign_key: "favorite_id"
+  has_many :collectors, class_name: "User", through: :collector_favorites
 end
--- recipe-swap-api/app/serializers/user_serializer.rb
+++ recipe-swap-api/app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password, :about, :profile_image
   has_many :recipes
+  # Yeehaw! Step through is not needed!!
+  # has_many :collector_favorites, foreign_key: "collector_id"
+  has_many :favorites, class_name: "Recipe", through: :collector_favorites
 end
