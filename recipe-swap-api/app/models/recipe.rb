ff594b70c32f0e5e42f4ffa7253e5fc70aef4add
Roy Mosby <ego.madking@gmail.com>
2021-06-21 21:13:42 -0400

    adds model associations

--- recipe-swap-api/app/models/recipe.rb
+++ recipe-swap-api/app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  belongs_to :original, class_name: "Recipe", optional: true
+  has_many :spinoffs, class_name: "Recipe", foreign_key: "original_id"
+  has_many :collector_favorites, foreign_key: "favorite_id"
+  has_many :collectors, class_name: "User", through: :collector_favorites
 end
--- recipe-swap-api/app/models/user.rb
+++ recipe-swap-api/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :recipes
+  has_many :collector_favorites, foreign_key: "collector_id"
+  has_many :favorites, class_name: "Recipe", through: :collector_favorites
+  
+  has_secure_password
 end
