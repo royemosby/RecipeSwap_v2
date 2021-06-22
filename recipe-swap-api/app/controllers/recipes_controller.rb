540f9aec2f263973dbe17afa2a4cd41585e45f23
Roy Mosby <ego.madking@gmail.com>
2021-06-22 16:15:33 -0400

    adds index, show, and stubs remaining actions

--- recipe-swap-api/app/controllers/recipes_controller.rb
+++ recipe-swap-api/app/controllers/recipes_controller.rb
@@ -1,2 +1,28 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+    render json: @recipes
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+    render json: @recipe
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :ingredients, :instructions, :servings, :user_id, :original_id)
+  end
 end
