515ff26a35292aa61c9d515e3f456f607613a635
Roy Mosby <ego.madking@gmail.com>
2021-06-22 16:13:53 -0400

    corrects params typo in create

--- recipe-swap-api/app/controllers/auth_controller.rb
+++ recipe-swap-api/app/controllers/auth_controller.rb
@@ -3,11 +3,12 @@ class AuthController < ApplicationController
   skip_before_action :authenticate, only: [:create]
 
   def create
-    @user = User.find_by(username: login_params[:username])
-    if @user && @user.athenticate(login_params[:password])
+    #byebug
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
       #encode_token resides in application controller
       @token = encode_token({user_id: @user.id})
-      render json: {user: UserSerializer.new(@user), jwt: token}
+      render json: {user: UserSerializer.new(@user), jwt: @token}
     else
       render json: {message: "Invalid username or password"}, status: :unauthorized
     end
