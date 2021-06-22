0fb56837f47b44e75b23d4f3070cae54120778c4
Roy Mosby <ego.madking@gmail.com>
2021-06-22 15:07:17 -0400

    adds authentication logic

--- recipe-swap-api/app/controllers/application_controller.rb
+++ recipe-swap-api/app/controllers/application_controller.rb
@@ -1,2 +1,49 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def encode_token(payload)
+    # TODO: keep secrets safe
+    JWT.encode(payload, "SuP3rS3cr37")
+  end
+  
+  def auth_header
+    request.headers['Authorization']
+  end
+  
+  def decoded_token
+    if auth_header
+      # auth_header returns: "Bearer <<token>>"
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, "SuP3rS3cr37", true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        # TODO: is there something else that can go here for client?
+        nil
+      end
+    end
+  end
+  
+  def current_user
+    if decoded_token
+      # eg: [{ "user_id"=>"2" }, { "alg"=>"HS256" }]
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    else
+
+    end
+  end
+
+  def logged_on?
+    !!current_user
+  end
+
+
+  def authenticate
+    render json: {message: 'Please log in'}, status: unauthorized unless logged_in?
+  end
+
+  #TODO: Should I do authorization in the app controller too?
+  ## all authenticated users create recipes, spinoffs, favorites
+  ## current_user only able to mod own profile, recipes, favorites
+
 end
