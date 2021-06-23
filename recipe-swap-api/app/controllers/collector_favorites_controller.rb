f2e70c2d57df8f0d8c2b3a481566bc49cdc9e493
Roy Mosby <ego.madking@gmail.com>
2021-06-23 16:00:24 -0400

    creates index, create, destroy actions

--- recipe-swap-api/app/controllers/collector_favorites_controller.rb
+++ recipe-swap-api/app/controllers/collector_favorites_controller.rb
@@ -1,2 +1,40 @@
 class CollectorFavoritesController < ApplicationController
+
+  #TODO: validate destroy actions
+  def index
+    user_id = current_user.id
+    @collector_favorites = CollectorFavorite.where("collector_id = ?", user_id)
+    if @collector_favorites &&@collector_favorites.count > 0
+      render json: @collector_favorites
+    else
+      render json: {message: "This user has no favorites"}
+    end
+  end
+
+  #TODO: enforce uniqueness
+  def create
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @user = User.find_by(id: params[:user_id])
+    
+    #byebug
+    if @user && @recipe && @user == current_user
+      @favorite = CollectorFavorite.create(favorite_id: @recipe.id, collector_id: @user.id)
+      render json: @favorite
+    else
+      render json: {error: "Something went wrong"}
+    end
+  end
+
+  #TODO: make proper!
+  def destroy
+    @favorite = CollectorFavorite.find_by(id: params[:id])
+    #byebug
+    if @favorite && @favorite.collector == current_user
+      @favorite.destroy
+      render json: {message: 'favorite has been removed'}
+    else
+      render json: {error: "Something went wrong"}
+    end
+  end
+
 end
