f29cbec7e7d087d5b4bf2a3d187ac296b3d9d326
Roy Mosby <ego.madking@gmail.com>
2021-06-25 17:25:41 -0400

    adds currentUser reducer

--- recipe-swap-frontend/src/reducers/index.js
+++ recipe-swap-frontend/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import { favorites } from "./favorites";
 import { recipes } from "./recipes";
 import { users } from "./users";
+import { currentUser } from "./currentUser";
 
 export default combineReducers({
+  currentUser,
   favorites,
   recipes,
   users
