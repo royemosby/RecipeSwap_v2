23228b08301085bd92e16bd37e1ee345f7debb58
Roy Mosby <ego.madking@gmail.com>
2021-06-25 07:45:54 -0400

    implements initial routing

--- recipe-swap-frontend/src/App.js
+++ recipe-swap-frontend/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import MainContainer from './main/MainContainer';
+import RecipeContainer from './recipes/RecipeContainer';
 import UsersContainer from './users/UsersContainer';
 import UserContainer from './users/UserContainer';
 import Header from './common/Header'
@@ -9,13 +11,18 @@ import Footer from './common/Footer'
 
 function App() {
   return (
-    <>
+    <div className="skeleton">
       <Header/>
-      <MainContainer/>
-      <UsersContainer/>
-      <UserContainer/>
+      <Router>
+        <div>
+          <Route exact path="/" component={MainContainer}/>
+          <Route path="/recipe" component={RecipeContainer}/>
+          <Route path="/user" component={UserContainer}/>
+          <Route path="/users" component={UsersContainer}/>
+        </div>
+      </Router>
       <Footer/>
-    </>
+    </div>
   );
 }
 
