75ade60734f347bf5974f49f4cf0d5391f384005
Roy Mosby <ego.madking@gmail.com>
2021-06-25 17:23:18 -0400

    adds nav links

--- recipe-swap-frontend/src/common/Header.js
+++ recipe-swap-frontend/src/common/Header.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <h1 className="prez">Header</h1>
+    <div>
+      <h1 className="prez">Header</h1>
+      <nav>
+        <NavLink to="/"
+                  className="linky"
+                  exact
+                  activeStyle={{background: 'lightblue'}}>
+        Home </NavLink>
+        <NavLink to="/recipe"
+                  className="linky"
+                  exact
+                  activeStyle={{background: 'lightblue'}}>
+        Recipe(hone w/params) </NavLink>
+        <NavLink to="/user"
+                  className="linky"
+                  exact
+                  activeStyle={{background: 'lightblue'}}>
+        User(hone w/params) </NavLink>
+        <NavLink to="/users"
+                  className="linky"
+                  exact
+                  activeStyle={{background: 'lightblue'}}>
+        Users </NavLink>
+      </nav>
+    </div>
   )
 }
 
