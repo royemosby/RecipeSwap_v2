5c537058536c01466f77ea853e4c75a225b773df
Roy Mosby <ego.madking@gmail.com>
2021-06-26 16:06:08 -0400

    init commit

---
+++ recipe-swap-frontend/src/adapters/adapterConfig.js
@@ -0,0 +1,7 @@
+const url = {base: "http://localhost:3000"}
+url.users = `${url.base}/users`
+url.recipes = `${url.base}/recipes`
+url.favorites = `${url.base}/collector_favorites`
+url.login = `${url.base}/login`
+
+export {url}
