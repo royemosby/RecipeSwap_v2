b7b341d83993af17f038f1b49a925c2e8c56e07e
Roy Mosby <ego.madking@gmail.com>
2021-06-27 08:20:06 -0400

    validates username uniqueness

--- recipe-swap-api/app/models/user.rb
+++ recipe-swap-api/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :collector_favorites, foreign_key: "collector_id"
   has_many :favorites, class_name: "Recipe", through: :collector_favorites
+
+  validates :username, uniqueness: true
   
   has_secure_password
 end
