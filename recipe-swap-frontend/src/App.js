3ba3ef5c904fa1c2e098cd3845a242b20515ccc0
Roy Mosby <ego.madking@gmail.com>
2021-06-27 17:57:15 -0400

    removes footer. Modifies switch routes to make them restful

--- recipe-swap-frontend/src/App.js
+++ recipe-swap-frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import MainContainer from './main/MainContainer';
 import RecipeContainer from './recipes/RecipeContainer';
 import UsersContainer from './users/UsersContainer';
 import UserContainer from './users/UserContainer';
 import Header from './common/Header'
-import Footer from './common/Footer'
 import AuthModal from './common/AuthModal'
 import {connect} from 'react-redux'
 
@@ -22,11 +20,13 @@ function App(props) {
       <Router>
         <div>
           <Header/>
-          <Route exact path="/" component={MainContainer}/>
-          <Route path="/recipe" component={RecipeContainer}/>
-          <Route path="/user" component={UserContainer}/>
-          <Route path="/users" component={UsersContainer}/>
-          <Footer/>
+          <Switch>
+            <Route exact path="/" component={MainContainer}/>
+            <Route path="/recipes/new" component={RecipeContainer}/>
+            <Route path="/recipes/:recipeId" component={RecipeContainer}/>
+            <Route path="/users/:username" component={UserContainer}/>
+            <Route path="/users" component={UsersContainer}/>
+          </Switch>
         </div>
         {renderAuthModal(props)}
       </Router>
