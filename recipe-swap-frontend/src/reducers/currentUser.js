9fb209ab7dcd104e935094f2cc2442e24e3d1f40
Roy Mosby <ego.madking@gmail.com>
2021-06-27 08:19:21 -0400

    adds authentication workflow

--- recipe-swap-frontend/src/reducers/currentUser.js
+++ recipe-swap-frontend/src/reducers/currentUser.js
@@ -1,6 +1,7 @@
 
 /* EXPECTED ACTIONS
-  LOGIN
+  SEND_CREDENTIALS
+  AUTHENTICATE
   LOGOUT
   CREATE_NEW_USER
   UPDATE_PROFILE
@@ -9,11 +10,15 @@
 */
 
 const initialState = {
+  id: "",
   token: "",
   username: "",
+  password: "",
   recipes: [],
   favorites: [],
-  loginModalOpen: false
+  loginModalOpen: false,
+  requesting: false,
+  loginError: ""
 }
 
 function currentUser(state = initialState, action){
@@ -22,6 +27,43 @@ function currentUser(state = initialState, action){
       return{...state, loginModalOpen: true}
     case "CLOSE_LOGIN_MODAL":
       return{...state, loginModalOpen: false}
+    case "UPDATE_UN":
+      return{...state, username: action.un}
+    case "UPDATE_PW":
+      return{...state, password: action.pw}
+    case "SEND_CREDENTIALS":
+      console.log("sending credentials...")
+      return {
+        ...state,
+        requesting: true
+      }
+    case "AUTHENTICATE":
+      if(action.response.message){
+        return{
+          ...state,
+          password: "",
+          loginError: action.response.message
+        }
+      } else {
+        console.dir(action.response)
+        return{
+          ...state,
+          id: action.response.user.id,
+          token: action.response.jwt,
+          username: action.response.user.username,
+          password: "", //let's not keep this
+          recipes: [...action.response.user.recipes],
+          favorites: [...action.response.user.favorites],
+          loginModalOpen: false,
+          requesting: false,
+          loginError: ""
+        }
+      }
+    case "LOG_OUT":
+      return{
+        ...state,
+        ...initialState
+      }
       default:
       return state
   }
