6ec0fe4eb762391665154c6205512da8d8d0a388
Roy Mosby <ego.madking@gmail.com>
2021-06-27 17:19:00 -0400

    populates user cards to userscontainer

--- recipe-swap-frontend/src/users/UsersContainer.js
+++ recipe-swap-frontend/src/users/UsersContainer.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react'
 import UserCard from './UserCard'
+import {connect} from 'react-redux'
 
 class UsersContainer extends Component{
+
+  renderUserCards(users){
+    return (
+      users.map(user => {
+        return <UserCard user={user}/>
+      })
+    )
+  }
   render(){
     return(
       <div className="skeleton">
         <h1>UsersContainer</h1>
-        <UserCard/>
+        {this.renderUserCards(this.props.users)}
       </div>
     )
   }
 }
 
-export default UsersContainer
+const mapStateToProps = (state) => {
+  return({
+    users: state.users
+  })
+}
+
+export default connect(mapStateToProps)(UsersContainer)
