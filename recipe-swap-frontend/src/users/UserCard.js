98aab335b3e53533c927a06f92f7e40c8954e2c0
Roy Mosby <ego.madking@gmail.com>
2021-06-27 17:17:53 -0400

    populates user card with params

--- recipe-swap-frontend/src/users/UserCard.js
+++ recipe-swap-frontend/src/users/UserCard.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const UserCard = () =>{
+const UserCard = ({user}) =>{
   return(
-    <div className="prez">UserCard</div>
+    <div className="prez">
+      <Link to={`/user/${user.username}`}>
+        <div>{user.username}</div>
+        <p>{user.about}</p>
+      </Link>
+    </div>
+
   )
 }
 
