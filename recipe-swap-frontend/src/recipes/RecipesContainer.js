b28b2b7a3697ccaa7fd8417f2551d9f1e7fe18f1
Roy Mosby <ego.madking@gmail.com>
2021-06-27 17:16:39 -0400

    populates recipes container with recipe cards

--- recipe-swap-frontend/src/recipes/RecipesContainer.js
+++ recipe-swap-frontend/src/recipes/RecipesContainer.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react'
 import RecipeCard from './RecipeCard'
+import {connect} from 'react-redux'
+
 
 class RecipesContainer extends Component{
+  renderRecipeCards = (recipes) =>{
+    return recipes.map((r, idx)=>{
+      return <RecipeCard key={idx} recipe={r}/>
+    })
+  }
+
   render(){
     return(
       <div className="skeleton">
         <h2>RecipesContainer</h2>
-        <RecipeCard/>
+        {this.renderRecipeCards(this.props.recipes)}
       </div>
     )
   }
 }
 
-export default RecipesContainer
+const mapStateToProps = (state) => {
+  return({
+    recipes: state.recipes
+  })
+}
+
+//TODO: move logic up to MainContainer and UserContainer so this
+// can be loaded onto user profile
+export default connect(mapStateToProps)(RecipesContainer)
