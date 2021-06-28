8e55932ab5472adc072cd5589ec0a94e0c897896
Roy Mosby <ego.madking@gmail.com>
2021-06-28 09:52:01 -0400

    adds empty recipes to each seed user

--- recipe-swap-frontend/src/reducers/users.js
+++ recipe-swap-frontend/src/reducers/users.js
@@ -1,12 +1,14 @@
 const user1 = {
   username: "Blanko",
   id: 'id_blanco',
+  recipes: [],
   about: 'this is a user(1) injected directly into users reducer'
 }
 
 const user2 = {
   username: "Stanko",
   id: 'id_stanko',
+  recipes: [],
   about: 'this is a user(2) injected directly into users reducer'
 }
 
