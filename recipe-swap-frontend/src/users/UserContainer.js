88f6c6a8a5b69e8479fd74d7aca6d1410afac3d7
Roy Mosby <ego.madking@gmail.com>
2021-06-28 07:41:57 -0400

    conditionally renders components in user container if current user

--- recipe-swap-frontend/src/users/UserContainer.js
+++ recipe-swap-frontend/src/users/UserContainer.js
@@ -1,21 +1,65 @@
 import React, {Component} from 'react'
+import UserFavorites from './UserFavorites'
 import UserNav from './UserNav'
 import UserProfile from './UserProfile'
 import RecipesContainer from '../recipes/RecipesContainer'
 import EditModal from './EditModal'
+import {connect} from 'react-redux'
 
 class UserContainer extends Component{
   render(){
+    
+    const targetUser = () => {
+      if (this.props.currentUser.username === this.props.match.params.username){
+        return this.props.currentUser
+      } else {
+        const found = this.props.users.find( user => {
+          return user.username === this.props.match.params.username
+        })
+        if (found instanceof Object){
+          return found
+        } else {
+          return {username: "User not found"}
+        }
+      }
+    }
+
+    const renderProfileSegments = () => {
+      if(Object.keys(targetUser()).includes("token") && targetUser().token.length > 0){
+        return (
+          <>
+            <UserNav user={targetUser()}/>
+            <UserProfile user={targetUser()}/>
+            <RecipesContainer user={targetUser()}/>
+            <UserFavorites />
+            <EditModal/>
+          </>
+        )
+      } else {
+        return (
+          <>
+            <UserProfile user={targetUser()}/>
+            <RecipesContainer user={targetUser()}/>
+          </>
+        )
+      }
+    }
+    
     return(
       <div className="skeleton">
-        <h1>UserContainer</h1>
-        <UserNav/>
-        <UserProfile/>
-        <RecipesContainer/>
-        <EditModal/>
+        <h1>{targetUser().username}</h1>
+        {renderProfileSegments()}
+        
       </div>
     )
   }
 }
 
-export default UserContainer
+const mapStateToProps = (state) => {
+  return({
+    users: state.users,
+    currentUser: state.currentUser
+  })
+}
+
+export default connect(mapStateToProps)(UserContainer)
