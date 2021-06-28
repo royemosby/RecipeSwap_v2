2e887b7a191bc83da7efac66c4c8cd9a3a993ab5
Roy Mosby <ego.madking@gmail.com>
2021-06-28 11:50:21 -0400

    adds props

--- recipe-swap-frontend/src/recipes/ShowRecipe.js
+++ recipe-swap-frontend/src/recipes/ShowRecipe.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const ShowRecipe = () => {
+const ShowRecipe = (props) => {
   return (
-    <div className="prez">recipeDetails</div>
+    <div className="prez">
+      <h1>Show Recipe (ID for now): </h1>
+      <h2> {props.match.params.recipeId}</h2>
+    </div>
   )
 }
 
