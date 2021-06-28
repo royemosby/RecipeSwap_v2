b828de2c6dc49729c846c6a8df70a02b90af04cc
Roy Mosby <ego.madking@gmail.com>
2021-06-28 07:42:09 -0400

    init commit

---
+++ recipe-swap-frontend/src/users/UserFavorites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserFavorites = (props) =>{
+  return (
+    <div className="prez">
+      <h2>User Favorites</h2>
+    </div>
+  )
+}
+
+export default UserFavorites
